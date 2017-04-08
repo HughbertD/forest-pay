64bb759d9c2d6009b6beb8f1493f4abd235039ae
Hugh Downer <hugh.downer@gmail.com>
2017-04-08 14:36:27 +0100

    Setup DatabaseSeeder for mock data to work with
     - Have deliberately kept this in one single master file rather than breaking into individual tables
     - Not enough configuration needed to warrant breaking apart etc, for a small amount of trivial data

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,13 @@
 <?php
 
-class DatabaseSeeder extends Seeder {
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    private $range = [
+        'low' => 1,
+        'high' => 10
+    ];
 
 	/**
 	 * Run the database seeds.
@@ -10,8 +17,48 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+        $faker = Faker::create();
+        foreach (range($this->range['low'], $this->range['high']) as $i) {
 
-		// $this->call('UserTableSeeder');
+            // new user
+            $user = User::create([
+                'username' => $faker->email,
+                'password' => Hash::make($faker->password()),
+            ]);
+
+            // attach profile
+            $user->profile()->save(new Profile([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+            ]));
+
+            // attach wallet
+            $user->wallet()->save(new Wallet([
+                'name' => $faker->domainWord
+            ]));
+
+            // Create between 1 and 2 bank accounts for user
+            $bankAccounts = [$this->getBankAccount($faker)];
+            if ($i >= $this->range['high'] / 2) {
+                $bankAccounts[] = $this->getBankAccount($faker);
+            }
+            $user->bankAccounts()->saveMany($bankAccounts);
+        }
 	}
+
+    /**
+     * Get a bank account
+     * @param \Faker\Generator $faker
+     * @return BankAccount
+     */
+	private function getBankAccount(\Faker\Generator $faker)
+    {
+        return new BankAccount([
+            'name' => ucwords($faker->domainWord),
+            'bank_name' => $faker->randomElement(['Lloyds TSB', 'Co-operate Bank', 'HSBC']),
+            'iban' => $faker->iban($faker->countryCode),
+            'beneficiary_name' => $faker->firstName . ' ' . $faker->lastName
+        ]);
+    }
 
 }
