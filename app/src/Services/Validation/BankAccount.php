9d95301fa3a7f5e635eb1c4b47ae49170eb8078e
Hugh Downer <hugh.downer@gmail.com>
2017-04-10 15:20:42 +0100

    Added todo

--- app/src/Services/Validation/BankAccount.php
+++ app/src/Services/Validation/BankAccount.php
@@ -4,6 +4,10 @@ namespace ForestPay\Services\Validation;
 
 class BankAccount extends BaseValidator
 {
+    /**
+     * @var array - rules for validating an account
+     * @todo: IBAN proper validation via checksum
+     */
     protected $rules = [
         'name' => 'required',
         'bank_name' => 'required',
